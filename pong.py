3afc3a8584c655b8945072992fe49dcb86d4fee6
tsur <tsur.doar@gmail.com>
2024-09-19 19:05:03 +0300

    final pong

--- pong.py
+++ pong.py
@@ -1,14 +1,20 @@
-x = int(input("enter x position: ")) 
-y = int(input("enter y position: ")) 
+import time
+import sys
+
 previous_y = 0
 previous_x = 0
 top_border = input("enter top borders: ").split(',')
+x = int(input("enter x position: ")) 
+y = int(input("enter y position: ")) 
+
 for i in range(0,len(top_border)):
     top_border[i] = int(top_border[i])
-bottom_border = [-1,-1]
-x_adder = 1
-y_adder = 1
+
+bottom_border = [0,0]
+x_adder = int(input("enter x gradient: ")) * -1
+y_adder = int(input("enter y gradient: ")) * -1
 board = []
+screen = {}
 
 def create():
     
@@ -17,44 +23,38 @@ def create():
     
     for i in range(0,top_border[1]):
         for l in range(0,top_border[0]):
-            screen[f'{i}'].append(0)
+            screen[f'{i}'].append(" ")
          
-        
-        
-        
-        
-        
-        
-
-screen = {
-    
-}
-create()
+def move_caret_up(n):
+    # Move the caret up by n lines
+    sys.stdout.write(f"\033[{n}A")
 
 def display(x,y):
     
-
-    screen[f"{previous_y}"][previous_x] = 0
-    screen[f'{y}'][x] = 1
+    screen[f"{previous_y}"][previous_x] = " "
+    screen[f'{y}'][x] = "o"
     
     for i in screen:
         print(screen[i])
-    
-    
-while True:
+    move_caret_up(len(screen))
 
-    input("")
-    print(x,y)
-    display(x,y)
-    previous_y = y
-    previous_x = x
-    
-    
-    if x >= top_border[0]-1 or x <= bottom_border[0]-1:
-        x_adder = x_adder * -1
+def _main_():
+    create()
 
-    if y >= top_border[1] -1 or y <= bottom_border[1]-1:
-        y_adder = y_adder * -1
+    while True:
 
-    x += x_adder
-    y += y_adder
+        time.sleep(0.01)
+
+        display(x,y)
+        previous_y = y
+        previous_x = x
+
+        if x >= top_border[0]-1 or x <= bottom_border[0]:
+            x_adder = x_adder * -1
+        if y >= top_border[1]-1 or y <= bottom_border[1]:
+            y_adder = y_adder * -1
+
+        x += x_adder
+        y += y_adder
+
+_main_()
